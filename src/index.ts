fcc86b5d69e30cb17789f38eb6af13f6e777ef6a
Taylor Reid <taylorsreid@gmail.com>
2025-01-21 20:25:06 -1000

    update exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 // this file is only for managing exports
 
 export * from './frames/index.js'
-export { KissConnection } from "./kissconnection.js"
-export { TNC } from "./tnc.js"
+export * from "./kissconnection.js"
+export * from "./tnc/tnc.js"
+export * from "./tnc/session.js"
 
 // "type" must be included for Bun compatibility, using Bun version 1.1.10 as of writing
 export type { Repeater } from "./misc.js"
